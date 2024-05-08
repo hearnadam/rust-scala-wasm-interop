a850210a237ae985a2300f39621625a6dcd4495f
Jordan Fjellman <8724130+jordanfjellman@users.noreply.github.com>
2024-05-08 17:16:37 -0500

    Initial script for setting up workers

---
+++ scripts/setup-workers.sh
@@ -0,0 +1,30 @@
+# Run script to create components and workers before execution
+# Example: ./setup-workers.sh "001"
+
+VERSION=$1 # Example: 001
+
+# Build our Rust components first
+cd ../rust
+
+# 1) The Http Client: Add the component and create an idle worker 
+cd http-client
+cargo component build --release
+golem-cli component add\
+  --component-name "http-client-$VERSION"\
+  ./target/wasm32-wasi/release/http_client.wasm
+golem-cli worker add --component-name "http-client-$VERSION" -w "example-http-client-$VERSION"
+cd ..
+
+
+# 2) The Stub: Add the component and create an idle worker
+cd http-client-stub
+golem-cli component add\
+  --component-name "http-client-stub-$VERSION"\
+  http-client-stub.wasm
+golem-cli worker add --component-name "http-client-stub-$VERSION" -w "example-http-client-stub-$VERSION"
+cd ..
+
+# Build our Scala components
+
+# TODO: We build all the scala things here, then profit.
+
