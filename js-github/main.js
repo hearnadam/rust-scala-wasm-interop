c8b69dde44269fb9864b95fd93800f3bc781aa96
Jordan Fjellman <8724130+jordanfjellman@users.noreply.github.com>
2024-05-08 18:35:33 -0500

    A little more JS

--- js-github/main.js
+++ js-github/main.js
@@ -1,10 +1,11 @@
-import * as http from  "request:http-client-stub/stub-http-client";
+import * as http from "request:http-client-stub/stub-http-client";
 
 export const api = {
-    gist(id) {
-        const workerId = "12344"
-        const httpClient = new http(`worker://${workerId}/get`)
-        const url = `https://gist.githubusercontent.com/hearnadam/${id}/raw/${id}/`
-        return httpClient.get(url);
-    }
+  gist(id) {
+    const componentId = "e59bd348-23c7-47b8-95e4-a5f027f807f3"
+    const workerId = "example-http-client-004"
+    const httpClient = new http(`worker://${componentId}/${workerId}`)
+    const url = `https://gist.githubusercontent.com/hearnadam/${id}/raw/${id}/`
+    return httpClient.get(url);
+  }
 }
