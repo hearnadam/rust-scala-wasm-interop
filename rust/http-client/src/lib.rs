61cd143a403b83655f18163c3e23c2956ffb0e4f
Jordan Fjellman <8724130+jordanfjellman@users.noreply.github.com>
2024-05-08 16:48:37 -0500

    Remove post and finish updating package paths

--- rust/http-client/src/lib.rs
+++ rust/http-client/src/lib.rs
@@ -1,6 +1,6 @@
 mod bindings;
 
-use crate::bindings::exports::request::sender::api::*;
+use crate::bindings::exports::request::http_client::api::*;
 use std::cell::RefCell;
 
 use reqwest::Client;
@@ -28,26 +28,5 @@ impl Guest for Component {
 
         Ok(response_body)
         // Ok(String::from("Result from 'get'"))
-    }
-
-    fn post(url: String) -> Result<String, String> {
-        println!("Sending a POST request to {} via HTTP", url);
-        let client = Client::builder()
-            .build()
-            .map_err(|err| format!("Failed to create the client!"))?;
-
-        let response = client
-            .get(url)
-            .send()
-            .map_err(|err| format!("Failed to send: {}", err))?;
-
-        let response_body = response
-            .text()
-            .map_err(|err| format!("Failed to convert to text: {}", err))?;
-
-        println!("Result: {:?}", response_body);
-
-        Ok(response_body)
-        // Ok(String::from("Result from 'post'"))
     }
 }
